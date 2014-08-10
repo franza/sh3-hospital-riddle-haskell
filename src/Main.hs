b8e12fde97bf0453872707dcf24410b71f818104
franza <nico.franza@gmail.com>
2014-08-11 01:23:44 +0400

    changed indents

--- src/Main.hs
+++ src/Main.hs
@@ -9,20 +9,20 @@ topRow = [1, 2, 3]
 
 clauses :: [Clause]
 clauses = [firstIsLargerThanSecond, secondIsTwiceTheThird, thirdIsSmallerThanFirst, 
-  fourthIsHalfOfTheFirst, notRepeated, threeAreNotInTopRow]
-  where
-  firstIsLargerThanSecond digits = digits !! 0 > digits !! 1
-  secondIsTwiceTheThird   digits = digits !! 1 == 2 * digits !! 2
-  thirdIsSmallerThanFirst digits = digits !! 2 < digits !! 0
-  fourthIsHalfOfTheFirst  digits = 
-    let dm = digits !! 0 `divMod` 2 
-    in digits !! 3 == fst dm && snd dm == 0
-  notRepeated             digits = sort digits == (sort $ nub digits)
-  threeAreNotInTopRow     digits = 
-    let filterLength n = filter ((== n) . length)
-        subs = filterLength 3 $ subsequences digits
-        inTopRow = (flip elem) topRow
-    in all (\xs -> not $ all inTopRow xs) subs
+    fourthIsHalfOfTheFirst, notRepeated, threeAreNotInTopRow]
+    where
+    firstIsLargerThanSecond digits = digits !! 0 > digits !! 1
+    secondIsTwiceTheThird   digits = digits !! 1 == 2 * digits !! 2
+    thirdIsSmallerThanFirst digits = digits !! 2 < digits !! 0
+    fourthIsHalfOfTheFirst  digits = 
+        let dm = digits !! 0 `divMod` 2 
+        in digits !! 3 == fst dm && snd dm == 0
+    notRepeated             digits = sort digits == (sort $ nub digits)
+    threeAreNotInTopRow     digits = 
+        let filterLength n = filter ((== n) . length)
+            subs = filterLength 3 $ subsequences digits
+            inTopRow = (flip elem) topRow
+        in all (\xs -> not $ all inTopRow xs) subs
 
 main = 
     let hasZeros = elem 0
