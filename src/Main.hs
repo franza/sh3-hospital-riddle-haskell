6d30f39fef2cb8e8a296cd2ca2622044044e71b3
franza <nico.franza@gmail.com>
2014-08-11 01:22:46 +0400

    changed indents, ifxed bug with div

--- src/Main.hs
+++ src/Main.hs
@@ -14,19 +14,20 @@ clauses = [firstIsLargerThanSecond, secondIsTwiceTheThird, thirdIsSmallerThanFir
   firstIsLargerThanSecond digits = digits !! 0 > digits !! 1
   secondIsTwiceTheThird   digits = digits !! 1 == 2 * digits !! 2
   thirdIsSmallerThanFirst digits = digits !! 2 < digits !! 0
-  fourthIsHalfOfTheFirst  digits = digits !! 3 == digits !! 0 `div` 2
+  fourthIsHalfOfTheFirst  digits = 
+    let dm = digits !! 0 `divMod` 2 
+    in digits !! 3 == fst dm && snd dm == 0
   notRepeated             digits = sort digits == (sort $ nub digits)
   threeAreNotInTopRow     digits = 
-    let subs = filter (\xs -> length xs == 3) $ subsequences digits
+    let filterLength n = filter ((== n) . length)
+        subs = filterLength 3 $ subsequences digits
         inTopRow = (flip elem) topRow
     in all (\xs -> not $ all inTopRow xs) subs
 
-reject :: (a -> Bool) -> [a] -> [a]
-reject f = filter (not . f)
-
 main = 
-  let sequence = reject (elem 0) $ map toDigits [1111..9999]
-      allClausesApplied = map (\x -> and $ map ($ x) clauses) sequence
-      filtered = filter snd $ zip sequence allClausesApplied
-      res = map fromDigits $ map fst filtered
-  in print res
+    let hasZeros = elem 0
+        sequence = filter (not . hasZeros) $ map toDigits [1111..9999]
+        allClausesApplied = map (\x -> and $ map ($ x) clauses) sequence
+        filtered = filter snd $ zip sequence allClausesApplied
+        res = map fromDigits $ map fst filtered
+    in print res
